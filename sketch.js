86c5e4c4731816a9bc9d46552a5de77ccd07de63
sharanrajthelegend26 <er.guhan@gmail.com>
2020-11-04 15:47:42 +0530

    added log 6

--- sketch.js
+++ sketch.js
@@ -33,6 +33,8 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(870,120,150, -PI/7);
+
 
     bird = new Bird(100,100);
 
@@ -58,6 +60,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
     platform.display();
